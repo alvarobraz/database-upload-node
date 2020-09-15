227ce57d0ef6a6c5981b38fcf7bb8e37a8783854
alvarobraz <alvarobraz83@gmail.com>
2020-09-15 17:44:36 -0300

    Finalizado desafio6 database upload

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepositiry = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepositiry.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepositiry.remove(transaction);
   }
 }
 
